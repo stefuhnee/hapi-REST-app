2ffe695a4820f88637edc6d5429868a33b02ac63
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-29 15:20:16 -0700

    added get route

--- routes/rest-routes.js
+++ routes/rest-routes.js
@@ -3,8 +3,26 @@
 const Dinosaur = require('../schema/dinosaur');
 const notFound = require('./not-found');
 
-module.exports = [{
-  method: '*',
-  path: '/{p*}',
-  handler: notFound
-}];
+module.exports = [
+  {
+    method: 'GET',
+    path: '/dinosaur',
+    handler: function(request, reply) {
+      Dinosaur.find({}, (err, dinosaur) => {
+        if (err) {
+          console.log('Error', err);
+          return reply('Error: extinction!!!!!!!');
+        }
+        return reply({
+          statusCode: 200,
+          message: 'The dinosaurs are roaming again!',
+          dinosaur: dinosaur
+        });
+      });
+    }
+  },
+  {
+    method: '*',
+    path: '/{p*}',
+    handler: notFound
+  }];
