bd149cce4f2ca92b25fad2cc191fdb6b6c2141fe
Ruben Sarkisyan <rubens24@uw.edu>
2016-05-29 16:07:51 -0700

    began writing tests

--- test/rest-test.js
+++ test/rest-test.js
@@ -9,3 +9,58 @@ const mongoose = require('mongoose');
 
 const dbPort = process.env.MONGLAB_URI;
 require('../server');
+
+describe('Dinosaur tests', () => {
+  after((done) => {
+    process.env.MONGOLAB_URI = dbPort;
+    mongoose.connection.db.dropDatabase(() => {
+      done();
+    });
+  });
+  it('should read a dinosaur', (done) => {
+    request('localhost:3000')
+    .get('/dinosaur/')
+    .end(err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+  it('should create a dinosaur' (done) => {
+    request('localhost:3000')
+    .post('/dinosaur/')
+    .send({name: 'post dinosaur', active: false})
+    .end(err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.body.name).to.eql('post dinosaur');
+      done();
+    });
+  });
+});
+
+describe('test that need Dinosaurs', () => {
+  let testDinosaur;
+  beforeEach((done) => {
+    let newDinosaur = new Dinosaur({name: 'test', active: true});
+    new Dinosaur.save = ((err, dinosaur) => {
+      testDinosaur = dinosaur;
+      done();
+    });
+  });
+  it('should update a Dinosaur', (done) => {
+    testDinosaur.name = 'updated';
+    request('localhost:3000')
+    .put('/dinosaur')
+    .end(err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.body.message).to.eql('Mutation successful! BETTER, FASTER, STRONGER!');
+      done();
+    });
+  });
+  it('should delete a Dinosaur', (done) => {
+    request('localhost:3000')
+    .delete('/dinosaur/' + testDinosaur)
+  })
+})
